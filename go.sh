f7949d76aa8b09a5115fe75c67e169c4a7bf3a68
Mushroom Hostage <mushroomhostage@yahoo.com>
2012-01-21 00:15:08 -0800

    Open source

--- go.sh
+++ go.sh
@@ -3,5 +3,5 @@ CLASSPATH=../craftbukkit-1.0.1-R1.jar javac *.java -Xlint:unchecked -Xlint:depre
 rm -rf me 
 mkdir -p me/exphc/Writable
 mv *.class me/exphc/Writable
-jar cf Writable.jar me/ *.yml
+jar cf Writable.jar me/ *.yml README ChangeLog *.java
 cp Writable.jar ../plugins/
